91e5886de217a6a8123df1cc07860ff52851e7c0
Athira PK <Athira PK@LAPTOP-F3JRHJJR>
2025-06-09 20:54:30 +0530

    Comments removed

--- GroceryApplication/src/main/java/utilities/ExtentReportUtility.java
+++ GroceryApplication/src/main/java/utilities/ExtentReportUtility.java
@@ -5,11 +5,11 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentReportUtility {
 	public static final ExtentReports extentReports = new ExtentReports();   
-	public synchronized static ExtentReports createExtentReports() {    //synch: for thread safety
-		ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html"); //create a path for saving the extent report
-		reporter.config().setReportName("GroceryApplication");  //to set report name
-		extentReports.attachReporter(reporter);   //to attach the spark report configuration to extent report instance to generate extent report
-		extentReports.setSystemInfo("Organization", "Obsqura");  //pass system level info
+	public synchronized static ExtentReports createExtentReports() { 
+		ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html"); 
+		reporter.config().setReportName("GroceryApplication");  
+		extentReports.attachReporter(reporter); 
+		extentReports.setSystemInfo("Organization", "Obsqura");  
 		extentReports.setSystemInfo("Name", "ATHIRA PK ");
 		return extentReports;   
 	}
